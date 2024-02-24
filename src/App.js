00b1d30b8c0ac5a027dd4e8d270cf4f075c9c03a
Shivam <shivambhardwaj.iimt@gmail.com>
2024-02-24 12:56:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+
+const Accordion = ({ title, body }) => {
+  const [show, setShow] = useState(false)
+  const toggleAccordition = () => setShow(!show)
+
+  return(
+    <div style={{border: '2px solid grey', marginBottom: '10px', width:"100%"}}>
+      <div onClick={toggleAccordition} style={{padding:"10px", backgroundColor:show ? "#ddd" :"#f1f1f1", cursor:"ponter", borderBottom:"1px solid green"  }}>
+        <h3 style={{color: show ?"Red":"orange"}}>{title}</h3>
+      </div>
+      {show && <div style={{padding:"10px"}}>{body}</div>}
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:"flex", margin:"10px", justifyContent:"space-between"}}>
+     <Accordion title="First" body="Thist is first accordion"/>
+     <Accordion title="Second" body="Thist is second accordion"/>
+     <Accordion title="Third" body="Thist is third accordion"/>
+     <Accordion title="Fourth" body="Thist is fourth accordion"/>
+     <Accordion title="Fifth" body="Thist is fifth accordion"/>
     </div>
   );
 }
